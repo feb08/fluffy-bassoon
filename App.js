310557edfff0b7ec98bf039a0486ad125cb6c201
Febri <febriaji8299@gmail.com>
2022-07-06 21:17:10 +0700

    layouting

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='input your goals' style={styles.input}/>
+        <Button title='Add Goals'/>
+      </View>
+      <View style={styles.listContainer}>
+        <Text>List Goals</Text>
+      </View>
     </View>
   );
 }
@@ -14,7 +20,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  inputContainer: {
+    paddingTop: 20,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+    flex: 1
+  },
+  input: {
+    borderWidth: 1,
+    height: 43,
+    width: '80%',
+    marginRight: 8,
+    borderRadius: 12,
+    paddingHorizontal: 10
+  },
+  listContainer: {
+    flex: 5,
+    borderTopWidth: 1,
+  }
 });
