0e1e38b28f9b7f92bbb885f0ef0b63fa1762ab11
Febri <febriaji8299@gmail.com>
2022-07-19 21:56:26 +0700

    handling taps with pressable component

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
@@ -13,6 +13,11 @@ export default function App() {
       text: enteredGoals, id: Math.random().toString()
     }]);
   }
+
+  function deleteHandler() {
+    console.log('Delete');
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -20,7 +25,7 @@ export default function App() {
       <View style={styles.listContainer}>
         <FlatList alwaysBounceVertical={false} showsVerticalScrollIndicator={false} data={courseGoal} renderItem={(itemData) => {
           return (
-            <GoalItem text={itemData.item.text}/>
+            <GoalItem text={itemData.item.text} onDeleteItem={deleteHandler}/>
           )
         }} keyExtractor={(item, index) => {
           return item.id
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,23 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import React from "react";
 
 const GoalItem = (props) => {
   return (
-    <View style={styles.listGoals}><Text style={styles.listText}>{props.text}</Text></View>
-  )
-}
+    <Pressable onPress={props.onDeleteItem}>
+      <View style={styles.listGoals}>
+        <Text style={styles.listText}>{props.text}</Text>
+      </View>
+    </Pressable>
+  );
+};
 
-export default GoalItem
+export default GoalItem;
 
 const styles = StyleSheet.create({
-    listGoals: {
-        borderRadius: 8,
-        padding: 8,
-        margin: 8,
-        backgroundColor: 'purple',
-        paddingHorizontal: 20
-      },
-      listText: {
-        color: 'white',
-      },
-})
+  listGoals: {
+    borderRadius: 8,
+    padding: 8,
+    margin: 8,
+    backgroundColor: "purple",
+    paddingHorizontal: 20,
+  },
+  listText: {
+    color: "white",
+  },
+});
